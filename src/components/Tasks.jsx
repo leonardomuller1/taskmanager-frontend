2408c6e66395672d89d6638724a5738a9dfd48ff
Leonardo Muller <leonardomuller78@gmail.com>
2024-05-15 09:35:56 -0300

    feat: add task item component

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./Tasks.scss";
+
+const Tasks = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:8000/tasks");
+      setTasks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  return (
+    <div className="tasks-container">
+      <h2>Minhas tarefas</h2>
+
+      <div className="last-tasks">
+        <h3>Últimas tarefas</h3>
+        <div className="tasks-list">
+          {tasks
+            .filter((tasks) => tasks.isCompleted === false)
+            .map((lastTask) => (
+              <p>{lastTask.description}</p>
+            ))}
+        </div>
+      </div>
+
+      <div className="completed-tasks">
+        <h3>Tarefas concluídas</h3>
+        <div className="tasks-list">
+          {tasks
+            .filter((tasks) => tasks.isCompleted === true)
+            .map((lastTask) => (
+              <p>{lastTask.description}</p>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tasks;
