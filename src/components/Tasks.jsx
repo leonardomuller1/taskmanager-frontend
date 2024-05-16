5428dcdc14da2bb3ecc356c6b1c557a724dc29af
Leonardo Muller <leonardomuller78@gmail.com>
2024-05-16 11:36:35 -0300

    perf: add last tasks and completed tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 import { useAlert } from "react-alert";
 
@@ -19,9 +19,18 @@ const Tasks = () => {
       alert.error("Algo deu errado.");
     }
   };
+
+  const lastTasks = useMemo(() => {
+    return tasks.filter((task) => task.isCompleted === false);
+  }, [tasks]);
+
+  const completedTask = useMemo(() => {
+    return tasks.filter((task) => task.isCompleted === true);
+  }, [tasks]);
   useEffect(() => {
     fetchTasks();
   }, []);
+
   return (
     <div className="tasks-container">
       <h2>Minhas Tarefas</h2>
@@ -29,29 +38,25 @@ const Tasks = () => {
         <h3>Últimas Tarefas</h3>
         <AddTask fetchTasks={fetchTasks} />
         <div className="tasks-list">
-          {tasks
-            .filter((task) => task.isCompleted === false)
-            .map((lastTask) => (
-              <TaskItem
-                key={lastTask._id}
-                task={lastTask}
-                fetchTasks={fetchTasks}
-              />
-            ))}
+          {lastTasks.map((lastTask) => (
+            <TaskItem
+              key={lastTask._id}
+              task={lastTask}
+              fetchTasks={fetchTasks}
+            />
+          ))}
         </div>
       </div>
       <div className="completed-tasks">
         <h3>Tarefas Concluídas</h3>
         <div className="tasks-list">
-          {tasks
-            .filter((task) => task.isCompleted)
-            .map((completedTask) => (
-              <TaskItem
-                key={completedTask._id}
-                task={completedTask}
-                fetchTasks={fetchTasks}
-              />
-            ))}
+          {completedTask.map((completedTask) => (
+            <TaskItem
+              key={completedTask._id}
+              task={completedTask}
+              fetchTasks={fetchTasks}
+            />
+          ))}
         </div>
       </div>
     </div>
